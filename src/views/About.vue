3904618dd2b5a45ac9e75d0b6c13b8a5a91e693c
Klim <jaschik26@yandex.ru>
2020-11-18 16:20:22 +0400

    carousel in progress

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>I'm about vue</h1>
+    
     <div class="carousel">
       <a class="carousel-item" href="#one!"><img src="https://lorempixel.com/250/250/nature/1"></a>
       <a class="carousel-item" href="#two!"><img src="https://lorempixel.com/250/250/nature/2"></a>
@@ -31,3 +31,18 @@ export default {
 
 };
 </script>
+
+<style lang="scss">
+
+  .about {
+    height: 100vh;
+  }
+
+  .carousel {
+   
+  }
+
+  .carousel-item {
+   min-width: 800px;
+  }
+</style>
